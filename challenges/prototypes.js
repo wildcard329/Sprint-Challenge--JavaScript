86b6a167dcd6853a03e04c6c9577f504794f39b9
wildcard329 <enkiduwildman@gmail.com>
2019-12-20 17:10:12 +0000

    completed functions.js, classes.js is returning syntax error which I am investigating

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -34,7 +34,7 @@ CuboidMaker.prototype.surfaceArea = function(length, width, height) {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = new CuboidMaker(4, 5, 5)
+let cuboid = new CuboidMaker(4, 5, 5)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
  console.log(cuboid.volume()); // 100
