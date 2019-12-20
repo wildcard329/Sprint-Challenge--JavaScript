a86b2b613a253377de0e5904cb80620af97165d4
wildcard329 <enkiduwildman@gmail.com>
2019-12-20 19:01:47 +0000

    completed assignment

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -102,7 +102,9 @@ const roster = graduates.forEach(function(student){
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.includes('Uni')
+const unisWithUni = universities.includes('Uni', 0)
+
+
 console.log(unisWithUni);
 
 
